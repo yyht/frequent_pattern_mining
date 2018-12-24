58f5f7c14cc57d877dcfe44e5e893aee2d2e8eba
天逸 <albert.xht@alibaba-inc.com>
2018-12-24 15:11:52 +0800

    fix mining ngram script.sh

--- mining_ngram.sh
+++ mining_ngram.sh
@@ -1,4 +1,4 @@
-python mining_ngram.py
+python mining_ngram.py \
  --train_file "/data/xuht/websiteanalyze-data-seqing20180821/seqing_train_20180821" \
  --stop_word_file "/data/xuht/stopwords-zh-master/stopwords-zh.txt" \
  --ouput_file "/data/xuht/websiteanalyze-data-seqing20180821/ngram/ngram" \
